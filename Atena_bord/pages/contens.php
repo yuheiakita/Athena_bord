b0f4280b1e98ec6718ba1c6325ec63c8f66924c8
yuheiakita <aktyuhi@gmail.com>
2019-04-08 01:11:45 +0900

    4/8 #1

---
+++ Atena_bord/pages/contens.php
@@ -0,0 +1,13 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lang="ja">
+  <html>
+    <head>
+      <title>おすすめのスレッド</title>
+    </head>
+    <body>
+     <div>
+      <p>ここにコメントを書き込む</p>
+     </div>
+    </body>
+  </html>
+</html>
---
+++ Atena_bord/pages/top.php
@@ -0,0 +1,22 @@
+<!DOCTYPE <!DOCTYPE html>
+<html lang="ja">
+  <html>
+    <head>
+      <div>
+        <title>Youtuberアングラコメ</title>
+
+        <!--画像入れる予定-->
+      </div>
+    </head>
+    <body>
+      <div>
+        <h1>Youtuberアングラコメ</h1>
+        <p>~Youtubeじゃでない情報を書き込もう~</p>
+      </div>
+      <div>
+          <h2>スレッド一覧</h2>
+          <a href="contens.html">おすすめのスレッド</a>
+      </div>
+    </body>
+  </html>
+</html>
