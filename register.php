5ad9a6517b1ef0607baf52d61a8fc4092f0460d5
yuheiakita <aktyuhi@gmail.com>
2019-05-19 15:24:43 +0900

    完成

--- register.php
+++ register.php
@@ -14,7 +14,6 @@
      <label for="password">password</label>
      <input type="password" name="password"><br>
      <button type="submit">Sign Up!</button>
-     <p>※パスワードは半角英数字をそれぞれ１文字以上含んだ、８文字以上で設定してください。</p>
    </form>
  </body>
 </html>
