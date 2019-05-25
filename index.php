dee6a3a8b1b5031521ad09fba9cdf9c16c7fcde5
yuheiakita <aktyuhi@gmail.com>
2019-05-26 06:13:27 +0900

    微修正

--- index.php
+++ index.php
@@ -8,10 +8,8 @@ if(isset($_SESSION['id'])&& $_SESSION['time'] + 3600>time()){
     $members = $db->prepare('SELECT* FROM members WHERE id=?');
     $members->execute(array($_SESSION['id']));
     $member =  $members->fetch();
-}else{
-    header('Location:login.php'); //ログインしていない場合は自動的にログインページへ飛ばされる
-    exit();
 }
+
 
 //投稿内容をDBへ保存
 if (!empty($_POST)){
@@ -33,33 +31,36 @@ $posts = $db->query('SELECT m.name, p.* FROM members m, posts p WHERE m.id=p.mem
 <html lang="ja">
  <head>
    <meta charset="utf-8">
+   <link rel="stylesheet" href="style.css">
+   <!-- <link rel="stylesheet" href="reset.css"> -->
    <title>AthenaBorad</title>
+   <p class="borad_naem">AthenaBorad</p>
  </head>
  <body>
-     <a href="register.php">新規登録</a>
-     <a href="login.php">ログイン</a>
-     <a href="logout.php">ログアウト</a>
+     <div class="button_conteiner">
+     <a href="register.php" class="button">新規登録</a>
+     <a href="login.php" class="button">ログイン</a>
+     <a href="logout.php" class="button">ログアウト</a>
+     </div>
+     </div>
+
+     <p class="login_name"><?php print(htmlspecialchars($member['name'],ENT_QUOTES));?>さん ログイン中</p>
    
-     <div class="container">
-    <h1>AthenaBorad</h1>
-    <form action=""   method="post">
-        <p><?php print(htmlspecialchars($member['name'],ENT_QUOTES)); ?>さん投稿してください</p>
-        </div>
+    <form action=""   method="post" >
         <div class="form-group">
-            <label for="content">本文</label>
-            <textarea cols="50" rows="5" name="message"></textarea>
-        </div>
-        <button type="submit" class="btn btn-info">投稿</button>
+            <textarea cols="50" rows="5" name="message" class="textarea" placeholder="今日の気分を投稿してください"></textarea>
+        </div class="post">
+        <button type="submit" class="btn-info">投稿</button>
     </form>
 </div>
 
 <?php foreach($posts as $post): ?>
 <div class="msg">
-    <p><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?>
+    <p class="msg_01"><?php print(htmlspecialchars($post['message'], ENT_QUOTES)); ?>
         <span class="name">(<?php print(htmlspecialchars($post['name'], ENT_QUOTES)); ?>)</span></p>
-    <p calss="day"><a href="view.php?id"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
+    <p calss="day"><a href="view.php?id" class="time"><?php print(htmlspecialchars($post['created'], ENT_QUOTES)); ?></a>
     <?php if ($_SESSION['id']==$post['member_id']): ?>
-    <a href="post_delete.php?id=<?php print(htmlspecialchars($post['id'])); ?>" style="=color:#F33;">削除</a>
+    <a href="post_delete.php?id=<?php print(htmlspecialchars($post['id'])); ?>" class="del">削除</a>
 <?php endif;?>
 </div>
 <?php endforeach; ?>
